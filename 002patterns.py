b803086eca06c9e4b6020b2f6329f9bb4f124390
SyedNajeeb <najeebsyedtech@gmail.com>
2020-03-03 21:53:51 +0530

    firstbranch add

---
+++ 002patterns.py
@@ -0,0 +1,39 @@
+# patterns
+
+for x in range(0, 10):
+    for y in range(0, x+1):
+        print('*', end='')
+    print('*', end='\n')
+
+for x in range(0, 10):
+    for y in range(0, 10-x):
+        print('*', end='')
+    print('*', end='\n')
+
+for x in range(0, 10):
+    for y in range(0, x):
+        print(' ', end='')
+    for z in range(0, 10-x):
+        print('*', end='')
+    print()
+
+for x in range(0, 10):
+    for y in range(0, 9-x):
+        print(' ', end='')
+    for z in range(0, x+1):
+        print('*', end='')
+    print()
+    
+for x in range(0, 10):
+    for y in range(0, x):
+        print(' ', end='')
+    for z in range(0, 10-x):
+        print('*', end=' ')
+    print()
+
+for x in range(0, 10):
+    for y in range(0, 9-x):
+        print(' ', end='')
+    for z in range(0, x+1):
+        print('*', end=' ')
+    print()
